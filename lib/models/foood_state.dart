53ea0f96371cafc330fd95399cd949efa9cd2046
BrucceVT <bvillena2000@gmail.com>
2023-12-06 23:22:58 -0500

    Adding simple food list

---
+++ lib/models/foood_state.dart
@@ -0,0 +1,9 @@
+class Alimento {
+  final String nombre;
+  final String materiaSeca;
+  final String proteinasTotal;
+  final String energiaMetabolizable;
+
+  Alimento(this.nombre, this.materiaSeca, this.proteinasTotal,
+      this.energiaMetabolizable);
+}
--- lib/presentation/food_list_screen.dart
+++ lib/presentation/food_list_screen.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:foodoptima/models/foood_state.dart';
 
-class FoodListScreen extends StatelessWidget {
+class FoodListScreen extends StatefulWidget {
   const FoodListScreen({super.key});
+  @override
+  _TablaScreenState createState() => _TablaScreenState();
+}
+
+class _TablaScreenState extends State<FoodListScreen> {
+  List<Alimento> alimentos = [
+    Alimento('Aceite de soya', '10g', '1g', '0.5Mcal/kg'),
+    Alimento('Alfalfa  heno,  plena flor', '20g', '2g', '0.7Mcal/kg'),
+    Alimento('Alfalfa ensilada', '30g', '3g', '0.9Mcal/kg'),
+    Alimento('Alfalfa fresca, plena floracion', '40g', '4g', '1.1Mcal/kg'),
+  ];
+
+  List<Alimento> selectedAlimentos = [];
+
+  final columns = [
+    'Alimentos',
+    'Materia Seca',
+    'Proteínas Total',
+    'Energía Metab. Mcal/kg'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -32,12 +53,42 @@ class FoodListScreen extends StatelessWidget {
               ))
         ],
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('Food List Screen'), Text('Final de la aplicacion')],
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+          columns: getColumns(columns),
+          rows: getRows(alimentos),
         ),
       ),
     );
   }
+
+  List<DataColumn> getColumns(List<String> columns) =>
+      columns.map((String column) => DataColumn(label: Text(column))).toList();
+
+  List<DataRow> getRows(List<Alimento> alimentos) =>
+      alimentos.map((Alimento alimento) {
+        final cells = [
+          alimento.nombre,
+          alimento.materiaSeca,
+          alimento.proteinasTotal,
+          alimento.energiaMetabolizable
+        ];
+        return DataRow(
+          selected: selectedAlimentos.contains(alimento),
+          onSelectChanged: (bool? selected) {
+            setState(() {
+              if (selected!) {
+                selectedAlimentos.add(alimento);
+              } else {
+                selectedAlimentos.remove(alimento);
+              }
+            });
+          },
+          cells: getCells(cells),
+        );
+      }).toList();
+
+  List<DataCell> getCells(List<dynamic> cells) =>
+      cells.map((data) => DataCell(Text('$data'))).toList();
 }
